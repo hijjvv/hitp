cefc0676ed01d9e5d22d7bbc54f5cf2a519cdd50
MO0121 <104887164+MO0121@users.noreply.github.com>
2022-07-15 18:38:20 +0900

    Update and rename BoardDo.java to Board.java

---
+++ Board.java
@@ -0,0 +1,51 @@
+package tp;
+
+public class Board {
+	
+	private	int aid;
+	private String title;
+	private String date;
+	private String content;
+	private int count;
+	private String img;
+
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	
+	
+}
